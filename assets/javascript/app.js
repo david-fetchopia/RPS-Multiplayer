a91def39ac7bea9d52236fd561e22bb69138aadf
DMa <davidweibinma@Weibins-MacBook-Pro-2.local>
2019-03-08 22:37:50 -0500

    initial changes

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyADtEdnJBj-4gu9Sq8zEQT3xe3J06XvBxc",
+    authDomain: "rps-multiplayer-game-bc98a.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-game-bc98a.firebaseio.com",
+    projectId: "rps-multiplayer-game-bc98a",
+    storageBucket: "rps-multiplayer-game-bc98a.appspot.com",
+    messagingSenderId: "143328548231"
+};
+firebase.initializeApp(config);
+
